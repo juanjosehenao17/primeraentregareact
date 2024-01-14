c32bff9c5139b2e815fee3016985cb8f50f59203
Juan Jose Henao <juanjosehenaomora@gmail.com>
2024-01-14 14:20:32 -0500

    modified:   package-lock.json

---
+++ src/Components/CartWidget.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./hojadeestilos.css"
+
+
+const CartWidget = () => {
+  return (
+    <div>
+      <i class="bi bi-cart-plus-fill"></i> 
+      
+    </div>
+  )
+} 
+
+export default CartWidget
